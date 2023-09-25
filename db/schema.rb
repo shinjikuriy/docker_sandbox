164fe7d81f352c7f7c1cd935237f12f5ecd34a78
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-09-25 22:49:57 +0900

    🗃️ Add constraints and organise columns

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_051720) do
     t.string "onyomis", array: true
     t.string "kunyomis", array: true
     t.string "nanoris", array: true
-    t.integer "stroke_count"
+    t.integer "stroke_count", null: false
     t.string "grade"
     t.string "jlpt"
     t.string "kanken"
@@ -57,14 +57,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_051720) do
   end
 
   create_table "tatsum_cdj", force: :cascade do |t|
-    t.string "level_for_kanji_and_signs_for_international_students"
-    t.integer "ranking_for_kanji_and_signs_for_international_students"
-    t.string "level_for_kanji_and_signs_for_general_learners"
-    t.integer "ranking_for_kanji_and_signs_for_general_learners"
-    t.string "level_for_kanji_and_signs_in_written_japanese"
-    t.integer "u_ranking_for_kanji_and_signs_in_written_japanese"
-    t.integer "overall_freq_ranking"
-    t.integer "fw_ranking_for_kanji_and_signs"
+    t.integer "level_for_kanji_and_signs_for_international_students", null: false
+    t.integer "ranking_for_kanji_and_signs_for_international_students", null: false
+    t.integer "level_for_kanji_and_signs_for_general_learners", null: false
+    t.integer "ranking_for_kanji_and_signs_for_general_learners", null: false
+    t.integer "level_for_kanji_and_signs_in_written_japanese", null: false
+    t.integer "u_ranking_for_kanji_and_signs_in_written_japanese", null: false
+    t.integer "overall_freq_ranking", null: false
+    t.integer "fw_ranking_for_kanji_and_signs", null: false
     t.string "the_former_jlpt_kanji_level"
     t.integer "specificity_level_in_humanities_and_arts_ha"
     t.integer "specificity_level_in_social_sciences_ss"
@@ -72,79 +72,73 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_051720) do
     t.integer "specificity_level_in_bio_medical_natural_sciences_bn"
     t.string "academic_kanji_and_limited_academic_domain_kanji"
     t.string "possible_literary_key_character"
-    t.string "type_of_character"
-    t.string "item"
-    t.integer "frequency"
-    t.float "standardized_freq_per_million_in_10_written_domains_fw"
-    t.string "fw_cumulative_text_coverage"
-    t.float "d"
-    t.integer "d_ranking"
-    t.float "uw_usage_coefficient_for_written_japanese"
-    t.integer "range"
-    t.float "lw_freq_per_million"
-    t.integer "lw_freq_ranking"
-    t.float "lp_freq_per_million"
-    t.integer "lp_freq_ranking"
-    t.float "he_freq_per_million"
-    t.integer "he_freq_ranking"
-    t.float "ah_freq_per_million"
-    t.integer "ah_freq_ranking"
-    t.float "pl_freq_per_million"
-    t.integer "pl_freq_ranking"
-    t.float "ec_freq_per_million"
-    t.integer "ec_freq_ranking"
-    t.float "se_freq_per_million"
-    t.integer "se_freq_ranking"
-    t.float "st_freq_per_million"
-    t.integer "st_freq_ranking"
-    t.float "bm_freq_per_million"
-    t.integer "bm_freq_ranking"
-    t.float "if_freq_per_million"
-    t.integer "if_freq_ranking"
-    t.integer "id_for_home_position"
-    t.string "ranking_by_freq_in_category"
-    t.integer "level_for_kanji_and_signs_for_international_students_i"
-    t.integer "level_for_kanji_and_signs_for_general_learners_i"
-    t.integer "level_for_kanji_and_signs_in_written_japanese_i"
+    t.string "type_of_character", null: false
+    t.string "item", null: false
+    t.integer "frequency", null: false
+    t.float "standardized_freq_per_million_in_10_written_domains_fw", null: false
+    t.string "fw_cumulative_text_coverage", null: false
+    t.float "d", null: false
+    t.integer "d_ranking", null: false
+    t.float "uw_usage_coefficient_for_written_japanese", null: false
+    t.integer "range", null: false
+    t.float "lw_freq_per_million", null: false
+    t.integer "lw_freq_ranking", null: false
+    t.float "lp_freq_per_million", null: false
+    t.integer "lp_freq_ranking", null: false
+    t.float "he_freq_per_million", null: false
+    t.integer "he_freq_ranking", null: false
+    t.float "ah_freq_per_million", null: false
+    t.integer "ah_freq_ranking", null: false
+    t.float "pl_freq_per_million", null: false
+    t.integer "pl_freq_ranking", null: false
+    t.float "ec_freq_per_million", null: false
+    t.integer "ec_freq_ranking", null: false
+    t.float "se_freq_per_million", null: false
+    t.integer "se_freq_ranking", null: false
+    t.float "st_freq_per_million", null: false
+    t.integer "st_freq_ranking", null: false
+    t.float "bm_freq_per_million", null: false
+    t.integer "bm_freq_ranking", null: false
+    t.float "if_freq_per_million", null: false
+    t.integer "if_freq_ranking", null: false
+    t.integer "id_for_home_position", null: false
+    t.string "ranking_by_freq_in_category", null: false
     t.index ["item"], name: "index_tatsum_cdj_on_item", unique: true
   end
 
   create_table "tatsum_vdrj", force: :cascade do |t|
-    t.string "word_level_for_international_students"
-    t.integer "word_ranking_for_international_students"
-    t.string "word_level_for_general_learners"
-    t.integer "word_ranking_for_general_learners"
-    t.string "word_level_for_written_japanese"
-    t.string "u_ranking_for_written_japanese_excluding_assumed_known_words"
-    t.integer "old_jlpt_level"
+    t.integer "word_level_for_international_students", null: false
+    t.integer "word_ranking_for_international_students", null: false
+    t.integer "word_level_for_general_learners", null: false
+    t.integer "word_ranking_for_general_learners", null: false
+    t.integer "word_level_for_written_japanese"
+    t.integer "u_ranking_for_written_japanese_excluding_assumed_known_words", null: false
+    t.integer "old_jlpt_level", null: false
     t.integer "specificity_level_in_humanities_and_arts_ha"
     t.integer "specificity_level_in_social_sciences_ss"
     t.integer "specificity_level_in_technological_natural_sciences_ss"
     t.integer "specificity_level_in_bio_medical_natural_sciences_bn"
-    t.string "possible_literary_keywords"
+    t.boolean "possible_literary_keywords", null: false
     t.string "word_tier_label"
-    t.string "lexeme"
-    t.string "standard_newspaper_orthography"
-    t.string "standard_reading_katakana"
-    t.string "part_of_speech"
-    t.string "word_origin_type"
+    t.string "lexeme", null: false
+    t.string "standard_newspaper_orthography", null: false
+    t.string "standard_reading"
+    t.string "part_of_speech", null: false
+    t.string "word_origin_type", null: false
     t.string "magazine_forms"
-    t.float "standardized_total_freq_per_million_in_10_written_domains_fw"
-    t.float "d"
-    t.integer "range"
-    t.float "lw_freq_per_million"
-    t.float "lp_freq_per_million"
-    t.float "he_freq_per_million"
-    t.float "ah_freq_per_million"
-    t.float "pl_freq_per_million"
-    t.float "ec_freq_per_million"
-    t.float "se_freq_per_million"
-    t.float "st_freq_per_million"
-    t.float "bm_freq_per_million"
-    t.float "oc_freq_per_million"
-    t.integer "word_level_for_international_students_i"
-    t.integer "word_level_for_general_learners_i"
-    t.integer "word_level_for_written_japanese_i"
+    t.float "standardized_total_freq_per_million_in_10_written_domains_fw", null: false
+    t.float "d", null: false
+    t.integer "range", null: false
+    t.float "lw_freq_per_million", null: false
+    t.float "lp_freq_per_million", null: false
+    t.float "he_freq_per_million", null: false
+    t.float "ah_freq_per_million", null: false
+    t.float "pl_freq_per_million", null: false
+    t.float "ec_freq_per_million", null: false
+    t.float "se_freq_per_million", null: false
+    t.float "st_freq_per_million", null: false
+    t.float "bm_freq_per_million", null: false
+    t.float "oc_freq_per_million", null: false
     t.index ["lexeme"], name: "index_vocabs_on_lexeme", unique: true
     t.index ["standard_newspaper_orthography"], name: "index_vocabs_on_standard_newspaper_orthography"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,22 @@ CSV.foreach(Rails.root.join('db/db_formatted/joyokanji_mod.tsv'), headers: true,
   Joyokanji.create_or_find_by(row.to_h)
 end
 
-headers = MifuneKanjidict.column_names.map(&:to_sym)
-headers.delete(:id)
-CSV.foreach(Rails.root.join('db/source/mifunetoshiro_kanjidict.txt'), col_sep: "\t") do |row|
-  att = headers.zip(row).to_h
-  att[:reg_on] = att[:reg_on]&.split('、')
-  att[:reg_kun] = att[:kun]&.split('、')
-  att[:onyomi] = att[:onyomi]&.split('、')
-  att[:kunyomi] = att[:kunyomi]&.split('、')
-  att[:nanori] = att[:nanori]&.split('、')
-  att[:meaning] = att[:meaning]&.split(';')
-  att[:compact_meaning] = att[:compact_meaning]&.split(';')
-  MifuneKanjidict.create_or_find_by(att)
+CSV.foreach(Rails.root.join('db/db_formatted/mifune_kanjidict_mod.tsv'), headers: true, col_sep: "\t") do |row|
+  row['joyo_onyomis'] = row['joyo_onyomis']&.split(',')
+  row['joyo_kunyomis'] = row['joyo_kunyomis']&.split(',')
+  row['onyomis'] = row['onyomis']&.split(',')
+  row['kunyomis'] = row['kunyomis']&.split(',')
+  row['nanoris'] = row['nanoris']&.split(',')
+  row['meanings'] = row['meanings']&.split(',')
+  row['compact_meanings'] = row['compact_meanings']&.split(',')
+  MifuneKanjidict.create_or_find_by(row.to_h)
 end
 
-CSV.foreach(Rails.root.join('db/source/tatsum_cdj_kanji_and_signs.tsv'), headers: true, col_sep: "\t") do |row|
+CSV.foreach(Rails.root.join('db/db_formatted/tatsum_cdj_kanji_signs_mod.tsv'), headers: true, col_sep: "\t") do |row|
   TatsumCdj.create_or_find_by(row.to_h)
 end
 
-CSV.foreach(Rails.root.join('db/source/tatsum_vdrj.tsv'), headers: true, col_sep: "\t", liberal_parsing: true) do |row|
+CSV.foreach(Rails.root.join('db/db_formatted/tatsum_vdrj_mod.tsv'), headers: true, col_sep: "\t", liberal_parsing: true) do |row|
+  row['possible_literary_keywords'] = false if row['possible_literary_keywords'] == 'false'
   TatsumVdrj.create_or_find_by(row.to_h)
 end
