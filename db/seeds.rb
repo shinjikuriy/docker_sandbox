9a51672d1074c27cbe31624573a637985e54aecf
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-09-26 14:58:01 +0900

    ♻️ Refactor redundant codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,14 @@
 require 'csv'
 
 CSV.foreach(Rails.root.join('db/db_formatted/joyokanji_mod.tsv'), headers: true, col_sep: "\t") do |row|
-  row['grapheme_olds'] = row['grapheme_olds']&.split(',')
-  row['radicals'] = row['radicals']&.split(',')
-  row['onyomis'] = row['onyomis']&.split(',')
-  row['kunyomis'] = row['kunyomis']&.split(',')
+  keys = %w[grapheme_olds radicals onyomis kunyomis]
+  keys.each { |key| row[key] = row[key]&.split(',') }
   Joyokanji.create_or_find_by(row.to_h)
 end
 
 CSV.foreach(Rails.root.join('db/db_formatted/mifune_kanjidict_mod.tsv'), headers: true, col_sep: "\t") do |row|
-  row['joyo_onyomis'] = row['joyo_onyomis']&.split(',')
-  row['joyo_kunyomis'] = row['joyo_kunyomis']&.split(',')
-  row['onyomis'] = row['onyomis']&.split(',')
-  row['kunyomis'] = row['kunyomis']&.split(',')
-  row['nanoris'] = row['nanoris']&.split(',')
-  row['meanings'] = row['meanings']&.split(',')
-  row['compact_meanings'] = row['compact_meanings']&.split(',')
+  keys = %w[joyo_onyomis joyo_kunyomis onyomis kunyomis nanoris meanings compact_meanings]
+  keys.each { |key| row[key] = row[key]&.split(',') }
   MifuneKanjidict.create_or_find_by(row.to_h)
 end
 
@@ -23,7 +16,7 @@ CSV.foreach(Rails.root.join('db/db_formatted/tatsum_cdj_kanji_signs_mod.tsv'), h
   TatsumCdj.create_or_find_by(row.to_h)
 end
 
-CSV.foreach(Rails.root.join('db/db_formatted/tatsum_vdrj_mod.tsv'), headers: true, col_sep: "\t", liberal_parsing: true) do |row|
+CSV.foreach(Rails.root.join('db/db_formatted/tatsum_vdrj_mod.tsv'), headers: true, col_sep: "\t") do |row|
   row['possible_literary_keywords'] = false if row['possible_literary_keywords'] == 'false'
   TatsumVdrj.create_or_find_by(row.to_h)
 end
